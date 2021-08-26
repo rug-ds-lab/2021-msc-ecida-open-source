4279fb9c29249f26571d9b33c2f1e82396dd2b89
Max Verbeek <m4xv3rb33k@gmail.com>
2021-08-26 08:44:17 +0200

    push some changes

--- src/cmd/ecli/main.go
+++ src/cmd/ecli/main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/docopt/docopt-go"
+
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/chartutil"
 )
 
 func run() error {
 	usage := `ECiDA CLI tool
 
 Usage:
-  ecli list <chart>`
+  ecli list <chart>
+  ecli package <chart> <destination>`
 
 	opts, err := docopt.ParseDoc(usage)
 
@@ -22,7 +27,38 @@ Usage:
 	if list, _ := opts.Bool("list"); list {
         chart, _ := opts.String("<chart>")
 		fmt.Printf("listing %s\n", chart)
+
+        helmChart, err := loader.Load(chart)
+
+        if err != nil {
+            return fmt.Errorf("failed to load chart: %w", err)
+        }
+
+        for annotation, value := range helmChart.Metadata.Annotations {
+            if strings.HasPrefix(annotation, "ecida") {
+                fmt.Printf("%s: %s\n", annotation, value)
+            }
+        }
 	}
+
+    if pkg, _ := opts.Bool("package"); pkg {
+        chart, _ := opts.String("<chart>")
+        destination, _ := opts.String("<destination>")
+
+        helmChart, err := loader.Load(chart)
+
+        if err != nil {
+            return fmt.Errorf("failed to load chart: %w", err)
+        }
+
+        path, err := chartutil.Save(helmChart, destination)
+
+        if err != nil {
+            return fmt.Errorf("failed to export chart to %s: %w", destination, err)
+        }
+        
+        fmt.Printf("wrote package to %s\n", path)
+    }
 
 	return nil
 }
--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module ecida-cli
 
 go 1.16
 
-require github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	helm.sh/helm/v3 v3.6.3
+)
